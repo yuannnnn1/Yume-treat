932ed8629f7716f966df3bde884d2f61a80b4439
ehsellers <ehsellers@outlook.com>
2026-07-16 10:27:40 +0900

    put header and footer into a folder

--- footer.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <footer>
-            <p>Contact Us: yumetreat@gmail.com</p>
-            <p>&copy; 2026, Yume Treat</p>
-        </footer>
-    </body>
-</html>
---
+++ view/footer.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <footer>
+            <p>Contact Us: yumetreat@gmail.com</p>
+            <p>&copy; 2026, Yume Treat</p>
+        </footer>
+    </body>
+</html>
