34121183e3f071854a5b74ae6d664e56d2b9c107
袁靖庭 <yuanjingting@ywlan031031139.sras.sic.shibaura-it.ac.jp>
2026-07-16 10:24:11 +0900

    add header

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YUME TREAT</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<header class="header">
+
+    <!-- Left -->
+    <div class="header-left">
+        <a href="#">
+            <i class="fa-solid fa-bars"></i>
+        </a>
+    </div>
+
+    <!-- Center -->
+    <div class="header-logo">
+        <a href="index.php">YUME TREAT</a>
+    </div>
+
+    <!-- Right -->
+    <div class="header-right">
+        <a href="search.php">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </a>
+
+        <a href="cart.php">
+            <i class="fa-solid fa-cart-shopping"></i>
+        </a>
+    </div>
+
+</header>
